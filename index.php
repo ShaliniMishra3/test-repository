dc614519bda0e3a251bf9503e31d865ec42090cc
Shalini Mishra <shalinimishraji2002@gmail.com>
2024-11-19 15:02:23 +0530

    Added header,footer,and upload image paths

--- index.php
+++ index.php
@@ -1,33 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Test File</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
-        .container img{
-            width:30%;
-        }
+    .container img {
+        width: 30%;
+    }
     </style>
 </head>
+
 <body>
-<!-- Navbar Start Here -->
-<!--Navbar Ends Here -->
+    <!-- Navbar Start Here -->
+    <?php include('includes/navbar.php');?>
+    <!--Navbar Ends Here -->
 
-<div class="col-lg-12">
-    <div class="container">
-    <img src="" alt="">
-    <img src="" alt="">
-    <img src="" alt="">
+    <div class="col-lg-12">
+        <div class="container">
+            <img src="img1.jpg" alt="">
+            <img src="img2.jpg" alt="">
+            <img src="img3.jpg" alt="">
+        </div>
     </div>
-</div>
 
 
-<!-- Footer Start Here -->
-<!--Footer Ends Here -->
+    <!-- Footer Start Here -->
+    <?php include('includes/footer.php');?>
+    <!--Footer Ends Here -->
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
